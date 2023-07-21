9bef6d9f24457357e8ab8de88e5e33ea3aabb4d2
Augusto Rodrigues <augustorodrigues14981@gmail.com>
2023-07-21 19:44:58 -0300

    primeiros passos com Node.js

--- nodes/ex001.js
+++ nodes/ex001.js
@@ -2,9 +2,21 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, res){
-    res.send('Seja bem-vindo ao meu app')
+    res.sendfile(__dirname + '/html/index.html')
+})
+
+app.get('/sobre', function(req, res) {
+    res.sendfile(__dirname + '/html/sobre.html')
+})
+
+app.get('/blog', function(req, res){
+    res.send('Bem vindo ao meu blog!')
+})
+
+app.get('/ola/:nome/:cargo', function(req, res){
+    res.send('<h1>Ola '+req.params.nome+'</h1>'+'<h2>Seu cargo: '+req.params.cargo+'</h2>')
 })
 
 app.listen(8081, function(){
-    console.log('Servido rodando!')
+    console.log('Servido rodando na url http://localhost:8081')
 })
